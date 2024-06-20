52536ee45b53637d1b7e606ddca4fc056ab775af
heyitsre7 <renukapohokar22@navgurukul.org>
2024-06-20 16:06:13 +0530

    reactBootstrap

---
+++ src/Login.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { Form, Button, Card, Container, Row, Col } from 'react-bootstrap';
+
+function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <Container className="mt-5">
+      <Row className="justify-content-center" >
+        <Col md={4}>
+          <Card>
+            <Card.Body>
+              <Card.Title className="text-center">Login</Card.Title>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formUsername">
+                  <Form.Label>User Name:</Form.Label>
+                  <Form.Control 
+                    type="text" 
+                    value={username} 
+                    onChange={(e) => setUsername(e.target.value)} 
+                  />
+                </Form.Group>
+                <Form.Group controlId="formPassword" className="mt-3">
+                  <Form.Label>Password:</Form.Label>
+                  <Form.Control 
+                    type="password" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit" className="mt-4 w-100">
+                  Login
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default LoginForm;
